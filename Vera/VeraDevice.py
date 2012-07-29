aa4c2d522e9bab2cc21e8ba0637b3281feb410cd
Rick Naething <naething@fake_email.fake>
2012-07-28 19:48:28 -0600

    Changed to support new devices: humidity, temperature, motion, door/window sensors, door locks

--- Vera/VeraDevice.py
+++ Vera/VeraDevice.py
@@ -1,5 +1,7 @@
+import pprint
 
 #-----------------------------------------------------------------------------
+# Generic Objects:
 class VeraDevice(object):
 
     def __init__ (self, callback, id, altid, category, name, parent, room, subcategory, status, comment):
@@ -13,63 +15,42 @@ class VeraDevice(object):
          self.subcategory = subcategory
          self.status      = status
          self.comment     = comment
+         
+         # not passed in:
+         self.watched     = []
 
     def __str__(self):
          return "%s" % self.name
 
     def update(self, d):
+        changed = False
+        
+        # see if a monitored variable changed
+        for watchedVar in self.watched:
+             if (watchedVar in d.keys()) and d[watchedVar] != getattr(self,watchedVar):
+                 changed = True
+                     
         for k in d.keys():
             try:
                 setattr(self, k, d[k])
             except AttributeError:
                 print self.name + ' does not have an attribute ' + k
 
+        if changed:
+            self.callback(self)
+
     @staticmethod
     def elements():
         return ['id', 'altid', 'category', 'name', 'parent', 'room', 'subcategory', 'status', 'comment']
 
-#-----------------------------------------------------------------------------
-class VeraSensor(VeraDevice):
-
-    def __init__ (self, base, lasttrip, tripped, armed):
-         super(VeraSensor,self).__init__(*base)
-         self.lasttrip = lasttrip
-         self.tripped  = tripped
-         self.armed    = armed
-         
-    @staticmethod
-    def elements():
-        return ['lasttrip', 'tripped', 'armed']
-
-#-----------------------------------------------------------------------------        
-class VeraHumiditySensor(VeraDevice):
-
-    def __init__ (self, base, humidity):
-         super(VeraHumiditySensor,self).__init__(*base)
-         self.humidity  = humidity
-
-    @staticmethod
-    def elements():
-        return ['humidity']
-
-#-----------------------------------------------------------------------------        
+#----------------------------------------------------------------------------- 
+# Category #2: Dimmable Lights       
 class VeraDimmableLight(VeraDevice):
 
     def __init__ (self, base, level):
          super(VeraDimmableLight,self).__init__(*base)
-         self.level  = level
-
-    def update(self, d):
-        update = False
-        # if I was turned ON/OFF:
-        if ('status' in d.keys()) and d['status'] != self.status:
-             update = True
-        # or my dimmed level changed:
-        elif ('level' in d.keys()) and d['level'] != self.level:
-             update = True
-        super(VeraDimmableLight,self).update(d)
-        if update:
-            self.callback(self)
+         self.level   = level
+         self.watched = ['status','level']
             
     def __str__(self):
         onOff = 'OFF' if self.status == '0' else 'ON'
@@ -79,20 +60,13 @@ class VeraDimmableLight(VeraDevice):
     def elements():
         return ['level']
 
-#-----------------------------------------------------------------------------        
+#-----------------------------------------------------------------------------
+# Category #3: Switch     
 class VeraSwitch(VeraDevice):
 
     def __init__ (self, base):
          super(VeraSwitch,self).__init__(*base)
-
-    def update(self, d):
-        update = False
-        # if I was turned ON/OFF:
-        if ('status' in d.keys()) and d['status'] != self.status:
-             update = True
-        super(VeraSwitch,self).update(d)
-        if update:
-            self.callback(self)
+         self.watched = ['status']
         
     def __str__(self):
         onOff = 'OFF' if self.status == '0' else 'ON'
@@ -102,7 +76,39 @@ class VeraSwitch(VeraDevice):
     def elements():
         return []
 
-#-----------------------------------------------------------------------------        
+#-----------------------------------------------------------------------------
+# Category #4: Security Sensor
+class VeraSensor(VeraDevice):
+
+    def __init__ (self, base, lasttrip, tripped, armed):
+         super(VeraSensor,self).__init__(*base)
+         self.lasttrip = lasttrip
+         self.tripped  = tripped
+         self.armed    = armed
+         self.watched  = ['armed', 'tripped']
+        
+    def __str__(self):
+        trip = 'CLEAR' if self.tripped == '0' else 'TRIPPED'
+        arm  = 'ARMED' if self.armed   == '1' else 'NOT_ARMED'
+        return(self.name + '.' + arm + '.' + trip)
+         
+    @staticmethod
+    def elements():
+        return ['lasttrip', 'tripped', 'armed']
+
+#-----------------------------------------------------------------------------
+# Category #5: Thermostat       
+class VeraThermostat(VeraDevice):
+
+    def __init__ (self, base):
+         super(VeraThermostat,self).__init__(*base)
+
+    @staticmethod
+    def elements():
+        return []
+
+#-----------------------------------------------------------------------------
+# Category #6: Camera
 class VeraCamera(VeraDevice):
 
     def __init__ (self, base, ip, url):
@@ -114,7 +120,65 @@ class VeraCamera(VeraDevice):
     def elements():
         return ['ip', 'url']    
          
-#-----------------------------------------------------------------------------        
+#----------------------------------------------------------------------------- 
+# Category #7: Door Lock
+class VeraDoorLock(VeraDevice):
+
+    def __init__ (self, base, locked):
+        super(VeraDoorLock,self).__init__(*base)  
+        self.locked  = locked
+        self.watched = ['locked']
+
+    def __str__(self):
+        lock = 'LOCKED' if self.locked == '1' else 'UNLOCKED'
+        return(self.name + '.' + lock)
+             
+    @staticmethod
+    def elements():
+        return ['locked']
+        
+
+#----------------------------------------------------------------------------- 
+# Category #8: Window Covering (dimmable light clone?)
+
+  
+#-----------------------------------------------------------------------------
+# Category #16: Humidity Sensor    
+class VeraHumiditySensor(VeraDevice):
+
+    def __init__ (self, base, humidity):
+         super(VeraHumiditySensor,self).__init__(*base)
+         self.humidity  = humidity
+         self.watched   = ['humidity'] 
+        
+    def __str__(self):
+        return(self.name + '.' + str(self.humidity))
+        
+    @staticmethod
+    def elements():
+        return ['humidity']
+
+#-----------------------------------------------------------------------------
+# Category #17: Temperature Sensor        
+class VeraTempSensor(VeraDevice):
+
+    def __init__ (self, base, temperature):
+         super(VeraTempSensor,self).__init__(*base)
+         self.temperature = temperature
+         self.watched     = ['temperature']
+        
+    def __str__(self):
+        return(self.name + '.' + str(self.temperature))
+
+    @staticmethod
+    def elements():
+        return ['temperature']
+
+#-----------------------------------------------------------------------------   
+# Category #21: Power Meter   
+
+#-----------------------------------------------------------------------------   
+# Category #22: Alarm Panel     
 class VeraAlarmPanel(VeraDevice):
 
     def __init__ (self, base):
@@ -124,17 +188,9 @@ class VeraAlarmPanel(VeraDevice):
     def elements():
         return []
         
-#-----------------------------------------------------------------------------        
-class VeraTempSensor(VeraDevice):
 
-    def __init__ (self, base):
-         super(VeraTempSensor,self).__init__(*base)
-
-    @staticmethod
-    def elements():
-        return []
-        
-#-----------------------------------------------------------------------------        
+#-----------------------------------------------------------------------------
+# Category #23: Alarm Partition 
 class VeraAlarmPartition(VeraDevice):
 
     def __init__ (self, base):
@@ -143,23 +199,4 @@ class VeraAlarmPartition(VeraDevice):
     @staticmethod
     def elements():
         return []
-                
-#-----------------------------------------------------------------------------        
-class VeraDoorLock(VeraDevice):
 
-    def __init__ (self, base):
-        super(VeraDoorLock,self).__init__(*base)  
-              
-    @staticmethod
-    def elements():
-        return []
-         
-#-----------------------------------------------------------------------------        
-class VeraThermostat(VeraDevice):
-
-    def __init__ (self, base):
-         super(VeraThermostat,self).__init__(*base)
-
-    @staticmethod
-    def elements():
-        return []
